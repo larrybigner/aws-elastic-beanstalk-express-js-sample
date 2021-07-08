6e56cbe7a4e84c9b789ba93c95006bb0b9637708
Larry Bigner <larrybigner@outlook.com>
2021-07-08 08:38:58 -0500

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('I made a change, bro!'));
+app.get('/', (req, res) => res.send('This change needs approval!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
